fc0dd9d1247f369bb7bc10471f4a8e4cbb36efc7
zwrSs <zwarios@gmail.com>
2013-06-03 00:46:55 +0200

    Przepisana siec - uczenie za pomoca Gaussa-Seidela

--- sciecNeuronowa/Program.cs
+++ sciecNeuronowa/Program.cs
@@ -15,75 +15,63 @@ namespace sciecNeuronowa
         /// </summary>
         [STAThread]
         static void Main()
-        { 
+        {
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            NNTest();
-        }
-
-        static void NNTest()
-        {
-            System.Console.WriteLine("Uszanowanko");
-            Network nn = new Network(2, 5, 1);
-            //Before teaching
-            System.Console.WriteLine("Before teaching:");
-            List<double> list = new List<double>();
-            list.Add(-1.0); list.Add(-1.0);
-            System.Console.WriteLine("Wynik F & F: " + nn.output(list)[0]);
-            list = new List<double>();
-            list.Add(-1.0); list.Add(1.0);
-            System.Console.WriteLine("Wynik F & T: " + nn.output(list)[0]);
-            list = new List<double>();
-            list.Add(1.0); list.Add(-1.0);
-            System.Console.WriteLine("Wynik T & F: " + nn.output(list)[0]);
-            list = new List<double>();
-            list.Add(1.0); list.Add(1.0);
-            System.Console.WriteLine("Wynik T & T: " + nn.output(list)[0]);
-
-            //Teaching
-            System.Console.WriteLine("Teaching...");
-            for (int i = 0; i < 10000; i++)
+            Network n = new Network(2, 1, 1);
+            double[] y = n.eval(new double[] { 1.0, 1.0 });
+            System.Console.WriteLine("Output = " + y[0]);
+            //n.teachMulti(new double[] { -1.0, 1.0 }, new double[] { -1.0 });
+            /*for (int i = 0; i < 100000; i++)
             {
-                List<double> oList = new List<double>();
-                oList.Add(-1.0);
-                list = new List<double>();
-                list.Add(-1.0); list.Add(-1.0);
-                nn.teachOne(list, oList);
-
-                oList = new List<double>();
-                oList.Add(-1.0);
-                list = new List<double>();
-                list.Add(-1.0); list.Add(1.0);
-                nn.teachOne(list, oList);
-
-                oList = new List<double>();
-                oList.Add(-1.0);
-                list = new List<double>();
-                list.Add(1.0); list.Add(-1.0);
-                nn.teachOne(list, oList);
-
-                oList = new List<double>();
-                oList.Add(1.0);
-                list = new List<double>();
-                list.Add(1.0); list.Add(1.0);
-                nn.teachOne(list, oList);
+                n.teach(new double[] { -1.0, -1.0 }, new double[] { -1.0 });
+                n.teach(new double[] { -1.0, 1.0 }, new double[] { -1.0 });
+                n.teach(new double[] { 1.0, -1.0 }, new double[] { -1.0 });
+                n.teach(new double[] { 1.0, 1.0 }, new double[] { 1.0 });
+            }*/
+            /*n.setWeights(new double[] {
+                0.098594092710998,
+                0.142027248431928,
+                0.168251298491528,
+                -0.303751077743045,
+                0.317479775149435,
+                0.316428999146291,
+                -0.282436690577179,
+                0.251041846015736,
+                0.892922405285977,
+                0.203223224556291,
+                1.555737942719387
+            });*/
+            {
+                n.bruteForceTeach(
+                    new double[][] {
+                        new double[] { -1, -1 }, 
+                        new double[] { -1, 1 },
+                        new double[] { 1, -1 },
+                        new double[] { 1, 1 }
+                    },
+                    new double[][] {
+                        new double[] { -1 },
+                        new double[] { 1 },
+                        new double[] { 1 },
+                        new double[] { -1 }
+                    });
             }
-
-            //After teaching
-            System.Console.WriteLine("After teaching:");
-            list = new List<double>();
-            list.Add(-1.0); list.Add(-1.0);
-            System.Console.WriteLine("Wynik F & F: " + nn.output(list)[0]);
-            list = new List<double>();
-            list.Add(-1.0); list.Add(1.0);
-            System.Console.WriteLine("Wynik F & T: " + nn.output(list)[0]);
-            list = new List<double>();
-            list.Add(1.0); list.Add(-1.0);
-            System.Console.WriteLine("Wynik T & F: " + nn.output(list)[0]);
-            list = new List<double>();
-            list.Add(1.0); list.Add(1.0);
-            System.Console.WriteLine("Wynik T & T: " + nn.output(list)[0]);
+            y = n.eval(new double[] { -1.0, -1.0 });
+            System.Console.WriteLine("-1 -1 = " + y[0]);
+            y = n.eval(new double[] { -1.0, 1.0 });
+            System.Console.WriteLine("-1 1 = " + y[0]);
+            y = n.eval(new double[] { 1.0, -1.0 });
+            System.Console.WriteLine("1 -1 = " + y[0]);
+            y = n.eval(new double[] { 1.0, 1.0 });
+            System.Console.WriteLine("1 1 = " + y[0]);
+            System.Console.WriteLine("");
+            foreach (double d in n.getWeights())
+            {
+                System.Console.WriteLine(d);
+            }
         }
+
     }
 }
